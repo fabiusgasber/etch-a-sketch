975fc3477ecc1e03091ffa090c0af4b4836eef25
Fabius Gasber <fabiusgasber@gmail.com>
2023-03-29 13:21:37 +0200

    Adjust height and width of cells

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function createGrid(){
     for(let i = 0; i < 16; i++){
         for(let j = 0; j < 16; j++){
         const div = document.createElement('div');
-        div.style.cssText = 'height:20px; width:20px; border: 1px solid black; margin: -1px; background-color: white;'
+        div.style.cssText = 'height:40px; width:40px; border: 1px solid black; margin: -1px; background-color: white;'
         container.appendChild(div);
         }
     }
